efd60264584f7b978a949c3db3b6458f5fcba13e
Werner Echezuria <werner_a_e@yahoo.es>
2018-05-08 16:55:00 -0300

    use Vec for wallets in ServerState struct and replace some types in Wallet Struct

--- src/server_state.rs
+++ src/server_state.rs
@@ -3,61 +3,63 @@ use bitprim::errors::*;
 use bitprim::executor::executor_destruct;
 use std::os::unix::io::AsRawFd;
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::collections::HashMap;
 use rocket::http::Status;
 use rocket::request::{self, FromRequest, Request};
 use std::process;
 use rocket::outcome::Outcome;
 use rocket::State;
 use std::sync::{Mutex, MutexGuard};
+use std::marker::Send;
 use wallet::Wallet;
 
-pub struct ServerState {
-  pub executor: Executor,
-  wallets: Mutex<HashMap<String, Box<Wallet>>>,
-  stopping: AtomicBool
+pub struct ServerState<T> {
+    pub executor: Executor,
+    wallets: Mutex<Vec<T>>,
+    stopping: AtomicBool,
 }
 
-impl ServerState {
-  pub fn new<O,E>(config_path: &str, out: &O, err: &E) -> Result<Self>
-    where O: AsRawFd, E: AsRawFd
-  {
-    let exec = Executor::new(config_path, out, err);
-    exec.initchain()?;
-    exec.run_wait()?;
-    Self{
-      executor: exec,
-      wallets: Mutex::new(HashMap::new()),
-      stopping: AtomicBool::new(false)
+impl<T : Wallet> ServerState<T> {
+    pub fn new<O, E>(config_path: &str, out: &O, err: &E) -> Result<Self>
+    where
+        O: AsRawFd,
+        E: AsRawFd,
+    {
+        let exec = Executor::new(config_path, out, err);
+        exec.initchain()?;
+        exec.run_wait()?;
+        Ok(Self {
+            executor: exec,
+            wallets: Mutex::new(vec![]),
+            stopping: AtomicBool::new(false),
+        })
     }
-  }
 
-  pub fn wallets_lock(&self) -> MutexGuard<Vec<Wallet>> {
-    self.wallets.lock().unwrap()
-  }
+    pub fn wallets_lock(&self) -> MutexGuard<Vec<T>> {
+        self.wallets.lock().unwrap()
+    }
 
-  pub fn graceful_stop(&self){
-    /* Due to how broken rocket's graceful shutdown is, we need to 
-     * do some low level cleanups in the executor and then call process::exit
-     * In an ideal world, you would be able to tell rocket's main thread to stop.
-     * Then program shutdown should follow, including all destructors.
-     */
-    self.stopping.store(true, Ordering::Relaxed);
-    unsafe{ executor_destruct(self.executor.raw) }
-    process::exit(0);
-  }
+    pub fn graceful_stop(&self) {
+        /* Due to how broken rocket's graceful shutdown is, we need to
+         * do some low level cleanups in the executor and then call process::exit
+         * In an ideal world, you would be able to tell rocket's main thread to stop.
+         * Then program shutdown should follow, including all destructors.
+         */
+        self.stopping.store(true, Ordering::Relaxed);
+        unsafe { executor_destruct(self.executor.raw) }
+        process::exit(0);
+    }
 }
 
-impl<'a,'r> FromRequest<'a, 'r> for &'r ServerState {
-	type Error = ();
+impl<'a, 'r, T : 'static + Wallet + Send> FromRequest<'a, 'r> for &'r ServerState<T> {
+    type Error = ();
 
-	#[inline(always)]
-	fn from_request(request: &'a Request<'r>) -> request::Outcome<&'r ServerState, ()> {
-		let state = request.guard::<State<ServerState>>()?;
-		if state.stopping.load(Ordering::Relaxed) {
-			Outcome::Failure((Status::ServiceUnavailable, ()))
-		}else{
-			Outcome::Success(state.inner())
-		}
-	}
+    #[inline(always)]
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<&'r ServerState<T>, ()> {
+        let state = request.guard::<State<ServerState<T>>>()?;
+        if state.stopping.load(Ordering::Relaxed) {
+            Outcome::Failure((Status::ServiceUnavailable, ()))
+        } else {
+            Outcome::Success(state.inner())
+        }
+    }
 }
--- src/wallet.rs
+++ src/wallet.rs
@@ -1,134 +1,151 @@
+use bitprim::executor::Executor;
+
 pub trait Wallet {
-  type Utxo;
+    type Utxo;
 
-  pub fn get_utxos(&self, exec: &Executor) -> Vec<Self::Utxo>;
+    fn get_utxos(&self, exec: &Executor) -> Vec<Self::Utxo>;
 }
 
 #[derive(Debug)]
-struct PlainWallet {
-  id: String,
-  version: String,
-  addresses: Vec<String>
+pub struct PlainWallet {
+    pub id: String,
+    pub version: String,
+    pub addresses: Vec<String>,
 }
 
 #[derive(Debug)]
-struct PlainUtxo {
-  prev_hash: String,
-  prev_index: u64,
-  address: String,
-  amount: u64
+pub struct PlainUtxo {
+    pub prev_hash: String,
+    pub prev_index: u64,
+    pub address: String,
+    pub amount: u64,
 }
 
 impl Wallet for PlainWallet {
-  type Utxo = PlainUtxo;
+    type Utxo = PlainUtxo;
 
-  pub fn get_utxos(&self, exec: &Executor) -> Vec<Self::Utxo> {
-    vec![PlainUtxo{
-      prev_hash: "abc".to_string(),
-      prev_index: 1,
-      address: "abc".to_string(),
-      amount: 100000000
-    }]
-  }
+    fn get_utxos(&self, _exec: &Executor) -> Vec<Self::Utxo> {
+        vec![
+            PlainUtxo {
+                prev_hash: "abc".to_string(),
+                prev_index: 1,
+                address: "abc".to_string(),
+                amount: 100000000,
+            },
+        ]
+    }
 }
 
 #[derive(Debug)]
 pub struct HdWallet {
-  pub id: String,
-  pub version: String,
-  pub addresses: Vec<HdAddress>,
-  pub xpub: String
+    pub id: String,
+    pub version: String,
+    pub addresses: Vec<HdAddress>,
+    pub xpub: String,
 }
 
 pub struct HdUtxo {
-  pub prev_hash: String,
-  pub prev_index: u64,
-  pub address: HdAddress,
-  pub amount: u64
+    pub prev_hash: String,
+    pub prev_index: u64,
+    pub address: HdAddress,
+    pub amount: u64,
 }
 
 impl Wallet for HdWallet {
-  type Utxo = HdUtxo;
+    type Utxo = HdUtxo;
 
-  pub fn get_utxos(&self, exec: &Executor) -> Vec<Self::Utxo> {
-    vec![HdUtxo{
-      prev_hash: "abc".to_string(),
-      prev_index: 1,
-      address: HdAddress{address: "abc".to_string(), path: vec![0,1,0]},
-      amount: 100000000
-    }]
-  }
+    fn get_utxos(&self, _exec: &Executor) -> Vec<Self::Utxo> {
+        vec![
+            HdUtxo {
+                prev_hash: "abc".to_string(),
+                prev_index: 1,
+                address: HdAddress {
+                    address: "abc".to_string(),
+                    path: vec![0, 1, 0],
+                },
+                amount: 100000000,
+            },
+        ]
+    }
 }
 
 #[derive(Debug)]
-pub struct Multisig {
-  pub id: String,
-  pub version: String,
-  pub addresses: Vec<HdAddress>,
-  pub xpubs: Vec<String>
-  pub signers: u64
+pub struct MultisigWallet {
+    pub id: String,
+    pub version: String,
+    pub addresses: Vec<HdAddress>,
+    pub xpubs: Vec<String>,
+    pub signers: u64,
 }
 
 pub struct MultisigUtxo {
-  pub prev_hash: String,
-  pub prev_index: u64,
-  pub address: HdAddress,
-  pub amount: u64
-  pub script_type: String,
-  pub multisig: MultisigDefinition
+    pub prev_hash: String,
+    pub prev_index: u64,
+    pub address: HdAddress,
+    pub amount: u64,
+    pub script_type: String,
+    pub multisig: MultisigDefinition,
 }
 
 pub struct MultisigDefinition {
-  pub signatures: Vec<String>,
-  pub m: u64,
-  pub pubkeys: Vec<PubkeyDefinition>
+    pub signatures: Vec<String>,
+    pub m: usize,
+    pub pubkeys: Vec<PubkeyDefinition>,
 }
 
 pub struct PubkeyDefinition {
-  pub address_n: Vec<u64>,1,1],
-  pub node: NodeDefiniton
+    pub address_n: Vec<u64>,
+    pub node: NodeDefinition,
 }
 
-pub struct NodeDefiniton {
-  pub chain_code: String,
-  pub depth: u64, 
-  pub child_num: u64, 
-  pub fingerprint: u64,
-  pub public_key: String
+pub struct NodeDefinition {
+    pub chain_code: String,
+    pub depth: u64,
+    pub child_num: u64,
+    pub fingerprint: u64,
+    pub public_key: String,
 }
 
-impl Wallet for HdWallet {
-  type Utxo = HdUtxo;
-  pub fn get_utxos(&self, exec: &Executor) -> Vec<Self::Utxo> {
-    let pubkeys = self.xpubs.iter().map(|xpub|{
-      PubkeyDefinition {
-        address_n: vec![0,0,1],
-        node: NodeDefinition {
-          chain_code: "Hello".to_string(),
-          depth: 0,
-          child_num: 0,
-          fingerprint: 0,
-          public_key: xpub
-        }
-      }
-    }).collect();
+impl Wallet for MultisigWallet {
+    type Utxo = MultisigUtxo;
 
-    vec![MultisigUtxo{
-      prev_hash: "abc".to_string(),
-      prev_index: 1,
-      address: HdAddress{address: "abc".to_string(), path: vec![0,1,0]},
-      amount: 100000000,
-      script_type: "SPENDMULTISIG",
-      multisig: MultisigDefinition {
-        signatures: vec!["","",""],
-        m: self.xpubs.len(),
-        pubkeys: pubkeys
-      }
-    }]
-  }
+    fn get_utxos(&self, _exec: &Executor) -> Vec<Self::Utxo> {
+        let pubkeys = self.xpubs
+            .iter()
+            .map(|xpub| PubkeyDefinition {
+                address_n: vec![0, 0, 1],
+                node: NodeDefinition {
+                    chain_code: "Hello".to_string(),
+                    depth: 0,
+                    child_num: 0,
+                    fingerprint: 0,
+                    public_key: xpub.to_string(),
+                },
+            })
+            .collect();
+
+        vec![
+            MultisigUtxo {
+                prev_hash: "abc".to_string(),
+                prev_index: 1,
+                address: HdAddress {
+                    address: "abc".to_string(),
+                    path: vec![0, 1, 0],
+                },
+                amount: 100000000,
+                script_type: "SPENDMULTISIG".to_string(),
+                multisig: MultisigDefinition {
+                    signatures: vec![String::new(), String::new(), String::new()],
+                    m: self.xpubs.len(),
+                    pubkeys: pubkeys,
+                },
+            },
+        ]
+    }
+}
 
 #[derive(Debug)]
 pub struct HdAddress {
-  pub address: String,
-  pub path: Vec<u64>
+    pub address: String,
+    pub path: Vec<u64>,
 }
