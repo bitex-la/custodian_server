3e633606da402ce8c830dd3d99f8addfd8060691
Werner Echezuria <werner_a_e@yahoo.es>
2018-05-08 16:54:57 -0300

    use PlainWallet for example function

--- src/main.rs
+++ src/main.rs
@@ -2,62 +2,60 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
+extern crate bitprim;
+extern crate ctrlc;
 extern crate libc;
 extern crate rocket;
-extern crate ctrlc;
-extern crate bitprim;
 mod server_state;
 mod wallet;
 use server_state::ServerState;
 use std::fs::File;
+use std::ops::DerefMut;
 use bitprim::PaymentAddress;
-use wallet::Wallet;
+use bitprim::explorer::OpaqueCollection;
+use wallet::PlainWallet;
 
-#[cfg(test)] mod tests;
+#[cfg(test)]
+mod tests;
 
 #[get("/")]
-fn hello(state: &ServerState) -> String {
-  let chain = state.executor.get_chain();
-  let mut wallets = state.wallets_lock();
-  wallets.push(Wallet::Legacy{
-    id: "hello".to_string(),
-    version: "hello".to_string(),
-    addresses: vec!["hello".to_string()]
-  });
+fn hello_plain_wallet(state: &ServerState<PlainWallet>) -> String {
+    let chain = state.executor.get_chain();
+    let mut wallets = state.wallets_lock();
+    wallets.deref_mut().push(PlainWallet {
+        id: "hello".to_string(),
+        version: "hello".to_string(),
+        addresses: vec!["hello".to_string()],
+    });
 
-  let addr = PaymentAddress::from_str("mqETuaBY9Tiq1asdsehEyQgCHe34SrXQs9");
-  let hist = chain.get_history(addr, 1000, 1).unwrap();
+    let addr = PaymentAddress::from_str("mqETuaBY9Tiq1asdsehEyQgCHe34SrXQs9");
+    let hist = chain.get_history(addr, 1000, 1).unwrap();
 
-  format!("Block: {:?}. Points: {:?}. Wallets: {:?}",
-          chain.get_last_height().expect("height"),
-          hist.count(),
-          *wallets)
+    format!(
+        "Block: {:?}. Points: {:?}. Wallets: {:?}",
+        chain.get_last_height().expect("height"),
+        hist.len(),
+        *wallets
+    )
 }
-/*
-#[post("/wallets", format = "application/json", data = "wallet")]
-fn create_plain_wallet(state: &ServerState) {
-}
-*/
 
 #[get("/stop")]
-fn stop(state: &ServerState) -> String {
-  state.graceful_stop();
-  format!("Stopping soon.")
+fn stop_plain_wallet(state: &ServerState<PlainWallet>) -> String {
+    state.graceful_stop();
+    format!("Stopping soon.")
 }
 
 fn main() {
-  let f = File::create("/dev/null")
-    .expect("/dev/null not available");
+    let f = File::create("/dev/null").expect("/dev/null not available");
 
-  let state = ServerState::new("./tests/btc-testnet.cfg", &f, &f)
-    .expect("Error creating State");
-  
-  ctrlc::set_handler(move || {
-    println!("Do not signal. Stop by visiting /stop");
-  }).expect("Error setting Ctrl-C handler");
-	
-  rocket::ignite()
-    .manage(state)
-    .mount("/", routes![hello, stop])
-    .launch();
+    let state : ServerState<PlainWallet> = ServerState::new("./tests/btc-testnet.cfg", &f, &f).expect("Error creating State");
+
+    ctrlc::set_handler(move || {
+        println!("Do not signal. Stop by visiting /stop");
+    }).expect("Error setting Ctrl-C handler");
+
+    rocket::ignite()
+        .manage(state)
+        .mount("/", routes![hello_plain_wallet, stop_plain_wallet])
+        .launch();
 }
