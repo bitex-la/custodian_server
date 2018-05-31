8a8ba238a41d4ee1e1193f6422882092a80430e2
werner <werner@yahoo.es>
2018-05-31 16:14:51 -0300

    add limit and since parameters for get_utxos

--- src/models/wallet.rs
+++ src/models/wallet.rs
@@ -10,13 +10,16 @@ pub trait Wallet {
     type Utxo;
     type A: ResourceAddress;
 
-    fn get_utxos(&self, exec: &Executor) -> Vec<Option<Self::Utxo>> {
+    fn get_utxos(&self, exec: &Executor, limit: Option<u64>, since: Option<u64>) -> Vec<Option<Self::Utxo>> {
         let explorer = exec.explorer();
 
         self.get_addresses().iter().flat_map(|address| {
             match PaymentAddress::from_str(&address.to_string()) {
                 Ok(valid_address) => {
-                    match explorer.address_unspents(valid_address, 10_000, 1000) {
+                    let last_height = exec.get_chain().get_last_height();
+                    match explorer.address_unspents(valid_address,
+                                                    limit.unwrap_or(10_000),
+                                                    since.unwrap_or(last_height.unwrap_or(1_000) - 1_000)) {
                         Ok(vec_received) => {
                             vec_received.into_iter().map(|received| {
                                 Some(self.construct_utxo(received, address))
