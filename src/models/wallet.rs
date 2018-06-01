316ccb419f67c28b99c3c0b404a5d214ad523289
werner <werner@yahoo.es>
2018-06-01 10:51:56 -0300

    use for loops instead of flat_map for clarity

--- src/models/wallet.rs
+++ src/models/wallet.rs
@@ -15,30 +15,20 @@ pub trait Wallet: std::marker::Sized + JsonApiModel + Clone + std::fmt::Debug {
     fn get_utxos(&self, exec: &Executor, limit: Option<u64>, since: Option<u64>) -> Vec<Self::Utxo> {
         let explorer = exec.explorer();
 
-        self.get_addresses().iter().flat_map(|address| {
-            match PaymentAddress::from_str(&address.to_string()) {
-                Ok(valid_address) => {
-                    let last_height = exec.get_chain().get_last_height();
-                    match explorer.address_unspents(valid_address,
-                                                    limit.unwrap_or(10_000),
-                                                    since.unwrap_or(last_height.unwrap_or(1_000) - 1_000)) {
-                        Ok(vec_received) => {
-                            vec_received.into_iter().map(|received| {
-                                self.construct_utxo(received, address)
-                            }).collect()
-                        },
-                        Err(err) => {
-                            println!("{:?}", err);
-                            vec![]
-                        }
+        let mut result: Vec<Self::Utxo> = vec![];
+        for address in self.get_addresses() {
+            if let Ok(valid_address) = PaymentAddress::from_str(&address.to_string()) {
+                let last_height = exec.get_chain().get_last_height();
+                if let Ok(vec_received) = explorer.address_unspents(valid_address,
+                                                limit.unwrap_or(10_000),
+                                                since.unwrap_or(last_height.unwrap_or(1_000) - 1_000)) {
+                    for received in vec_received {
+                        result.push(self.construct_utxo(received, address))
                     }
-                },
-                Err(err)         => { 
-                    println!("{:?}", err);
-                    vec![]
                 }
             }
-        }).collect()
+        }
+        result
     }
 
     fn construct_utxo(&self, received: Received, address: &Self::RA) -> Self::Utxo;
