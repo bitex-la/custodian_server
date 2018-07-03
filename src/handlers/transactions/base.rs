68f7eec65b7a9bfcce0c8fdf1e4a2bb3d63c6ae1
werner <werner@yahoo.es>
2018-07-03 11:39:25 -0300

    Match by boolean value in broadcast transaction

--- src/handlers/transactions/base.rs
+++ src/handlers/transactions/base.rs
@@ -1,16 +1,14 @@
 use rocket::http::Status;
 use rocket::response::status;
-use handlers::handler::{parse_to_value, JsonResult};
+use handlers::handler::{JsonResult};
 use rocket_contrib::{Json, Value};
-use jsonapi::model::*;
-use bitprim::hash::Hash;
 use server_state::ServerState;
 
 #[post("/transactions/broadcast", format = "application/json", data = "<hash>")]
 pub fn broadcast(state: &ServerState, hash: String) -> JsonResult {
     let chain = state.executor.get_chain();
     match chain.broadcast(&hash) {
-        Ok(_) => Ok(Json(Value::Null)),
-        Err(err) => Err(status::Custom(Status::InternalServerError, err.to_string()))
+        true => Ok(Json(Value::Null)),
+        false => Err(status::Custom(Status::InternalServerError, "Error broadcasting transaction".to_string()))
     }
 }
