dcb0821aedfb6ee59b067e5bf346d4f9b376ac42
werner <werner@yahoo.es>
2018-09-19 14:56:06 -0300

    Return a hash after broadcasting transaction

--- src/handlers/transactions/base.rs
+++ src/handlers/transactions/base.rs
@@ -1,14 +1,9 @@
-use rocket::http::Status;
-use rocket::response::status;
-use handlers::handler::{JsonResult};
-use rocket_contrib::{Json, Value};
+use rocket_contrib::{Json};
+use bitprim::hash::Hash;
 use server_state::ServerState;
 
 #[post("/transactions/broadcast", format = "application/json", data = "<hash>")]
-pub fn broadcast(state: &ServerState, hash: String) -> JsonResult {
+pub fn broadcast(state: &ServerState, hash: String) -> Json<Hash> {
     let chain = state.executor.get_chain();
-    match chain.broadcast(&hash) {
-        true => Ok(Json(Value::Null)),
-        false => Err(status::Custom(Status::InternalServerError, "Error broadcasting transaction".to_string()))
-    }
+    Json(chain.broadcast(&hash))
 }
