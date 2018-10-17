973e5f95999f8587728741e5ab488e4fb7c469fd
werner <werner@yahoo.es>
2018-10-17 13:49:03 -0300

    remove trezor-agent command from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -19,4 +19,4 @@ else
 fi
 
 cargo build --release --features $CURRENCY
-trezor-agent $HOME/.ssh/config -- scp target/release/custodian_server $USER@$IP:/home/ubuntu/apps/custodian_server/$CURRENCY 
+scp target/release/custodian_server $USER@$IP:/home/ubuntu/apps/custodian_server/$CURRENCY 
